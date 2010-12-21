496688860521b0fb489a81b73e4a4865ac2e22c3
Hubert Wong <hubertwong@hubert-wongs-macbook.local>
2010-12-20 17:01:59 -0800

    a really basica template with 1 scaffold and a basic css template. layout broken out into mini subfiles.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Zenfocus::Application.config.secret_token = 'ef1906a97ed3f227545b7ca451d6aa337648a054518186a05e292302de47e4319cf0f6843aaebb6b53a574f95ec1c881299c9b52ad045e47dac9bfcb8e09c4ae'
